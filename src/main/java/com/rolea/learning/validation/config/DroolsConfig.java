5de279c687d423ad41edbf46d6e72e78bf2a4beb
Alex Rolea <alexandru.rolea@softvision.com>
2020-06-22 11:02:05 +0300

    Small refactorings

--- src/main/java/com/rolea/learning/validation/config/DroolsConfig.java
+++ src/main/java/com/rolea/learning/validation/config/DroolsConfig.java
@@ -5,41 +5,32 @@ import org.kie.api.KieServices;
 import org.kie.api.builder.KieBuilder;
 import org.kie.api.builder.KieFileSystem;
 import org.kie.api.builder.KieModule;
+import org.kie.api.io.Resource;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSessionsPool;
 import org.kie.internal.io.ResourceFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ResourceLoader;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Arrays;
-
-import static org.springframework.core.io.support.ResourcePatternUtils.getResourcePatternResolver;
+import java.util.Optional;
 
 @Configuration
 @Slf4j
 public class DroolsConfig {
 
-    @Autowired
-    private ResourceLoader resourceLoader;
+    @Value("${rules.directory}")
+    private String rulesDirectory;
 
     @Bean
-    public KieContainer kieContainer() throws IOException {
+    public KieContainer kieContainer() {
         KieServices kieServices = KieServices.Factory.get();
         KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
 
-        Arrays.stream(getResourcePatternResolver(resourceLoader).getResources("classpath:rules/*"))
-                .forEach(resource -> {
-                    log.info("Loading rule file: {}", resource.getFilename());
-                    try {
-                        kieFileSystem.write(ResourceFactory.newFileResource(new File(resource.getURI())));
-                    } catch (IOException e) {
-                        log.warn("Unable to load file: {}", resource.getFilename());
-                    }
-                });
+        File rulesDirectory = getRulesDirectory();
+        loadRules(kieFileSystem, rulesDirectory);
 
         KieBuilder kieBuilder = kieServices.newKieBuilder(kieFileSystem);
         kieBuilder.buildAll();
@@ -49,8 +40,26 @@ public class DroolsConfig {
     }
 
     @Bean
-    public KieSessionsPool kieSessionsPool() throws IOException {
+    public KieSessionsPool kieSessionsPool() {
         return kieContainer().newKieSessionsPool(10);
+    }
+
+    private File getRulesDirectory() {
+        return Optional.ofNullable(getClass().getClassLoader().getResource(rulesDirectory))
+                .map(directoryUrl -> new File(directoryUrl.getFile()))
+                .orElseThrow(() -> new RuntimeException("Unable to locate rule directory"));
+    }
+
+    private void loadRules(KieFileSystem kieFileSystem, File rulesDirectory) {
+        Optional.ofNullable(rulesDirectory.list())
+                .ifPresent(ruleFiles -> Arrays.stream(ruleFiles)
+                        .forEach(ruleFile -> kieFileSystem.write(loadRule(rulesDirectory, ruleFile))));
+    }
+
+    private Resource loadRule(File folder, String ruleFile) {
+        String currentRule = folder.getPath() + File.separator + ruleFile;
+        log.info("Loading rule {}", currentRule);
+        return ResourceFactory.newFileResource(new File(currentRule));
     }
 
 }
